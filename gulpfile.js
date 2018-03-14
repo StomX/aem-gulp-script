f97e7403f7131373a6f37a3c3cda407f5bdc01de
주재만 <stomx@hivelab.co.kr>
2018-03-14 16:31:33 +0900

    UPDATE > Code refactoring
    Squashed commit of the following:
    
    commit cc09aa4edb547ad7c642199d3b73a271f72fa4fd
    Author: 주재만 <stomx@hivelab.co.kr>
    Date:   Wed Mar 14 16:26:50 2018 +0900
    
        UPDATE > package.json
    
    commit e1dd9f4d068b7b729b951816d78ee3ba84c7f4e0
    Author: 주재만 <stomx@hivelab.co.kr>
    Date:   Wed Mar 14 16:26:37 2018 +0900
    
        UPDATE > Gulpfile
    
    commit 37e08968fb2930694dae04b3e14526f42aacd49d
    Author: 주재만 <stomx@hivelab.co.kr>
    Date:   Wed Mar 14 16:26:11 2018 +0900
    
        UPDATE > .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,88 @@
-const $ = require( 'gulp-load-plugins' )( {
-        pattern: [ 'gulp*', 'gulp-*', 'gulp.*', '@*/gulp{-,.}*', 'fs' ]
-    } ),
-    init = JSON.parse( $.fs.readFileSync( './setting.json' ) );
-
-$.gulp.task( 'default', function () {} );
-
+const $    = require( 'gulp-load-plugins' )( {
+												 pattern: [ 'gulp*', 'gulp-*', 'gulp.*', '@*/gulp{-,.}*', 'fs' ]
+											 } ),
+	  init = JSON.parse( $.fs.readFileSync( './setting.json' ) );
+$.gulp.task( 'default', function () {
+} );
 $.gulp.task( 'compile:js', function () {
-    return $.gulp.src( 'jcr_root/**/devjs/{*,}/' )
-        .pipe( $.flatmap( function ( stream, dir ) {
-            return $.gulp.src( dir.path + '/*.js' )
-                .pipe( $.sourcemaps.init() )
-                .pipe( $.concat( 'main.js' ) )
-                .pipe( $.uglify() )
-                .pipe( $.rename( function ( path ) {
-                    var dirname = dir.path.split( 'jcr_root\\' )[ 1 ];
-                    path.dirname = 'jcr_root/' + dirname.replace( 'devjs', 'js/' ).replace(/\\/g,'/');
-                } ) )
-                .pipe( $.sourcemaps.write() )
-                .pipe( $.gulp.dest( './' ) );
-        } ) );
+	return $.gulp.src( 'jcr_root/**/devjs/{*,}/' )
+			.pipe( $.flatmap( function ( stream, dir ) {
+				return $.gulp.src( dir.path + '/*.js' )
+						.pipe( $.sourcemaps.init() )
+						.pipe( $.concat( 'main.js' ) )
+						.pipe( $.uglify() )
+						.pipe( $.rename( function ( path ) {
+							var dirname  = dir.path.split( 'jcr_root\\' )[ 1 ];
+							path.dirname = 'jcr_root/' + dirname.replace( 'devjs', 'js/' ).replace( /\\/g, '/' );
+						} ) )
+						.pipe( $.sourcemaps.write() )
+						.pipe( $.gulp.dest( './' ) );
+			} ) );
 } );
-
 $.gulp.task( 'compile:sass', function () {
-    return $.gulp.src( 'jcr_root/**/scss/{*,}/' )
-        .pipe( $.sourcemaps.init() )
-        .pipe( $.sass() )
-        .pipe( $.cleanCss( {
-            format: {
-                breaks: { // controls where to insert breaks
-                    afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
-                    afterRuleEnds: true, // controls if a line break comes after a rule ends; defaults to `false`
-                }
-            }
-        } ) )
-        .pipe( $.sourcemaps.write() )
-        .pipe( $.rename( function ( path ) {
-            var dirname = path.dirname;
-            path.dirname = dirname.replace( 'scss', 'css/' );
-            path.basename = init.output.basename;
-        } ) )
-        .pipe( $.gulp.dest( 'jcr_root/' ) );
+	return $.gulp.src( 'jcr_root/**/scss/{*,}/' )
+			.pipe( $.sourcemaps.init() )
+			.pipe( $.sass() )
+			.pipe( $.cleanCss( {
+								   format: {
+									   breaks: { // controls where to insert breaks
+										   afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
+										   afterRuleEnds:  true // controls if a line break comes after a rule ends; defaults to `false`
+									   }
+								   }
+							   } ) )
+			.pipe( $.sourcemaps.write() )
+			.pipe( $.rename( function ( path ) {
+				var dirname   = path.dirname;
+				path.dirname  = dirname.replace( 'scss', 'css/' );
+				path.basename = init.output.basename;
+			} ) )
+			.pipe( $.gulp.dest( 'jcr_root/' ) );
 } );
-
 $.gulp.task( 'compile:less', function () {
-    return $.gulp.src( 'jcr_root/**/less/{*,}/' )
-        .pipe( $.sourcemaps.init() )
-        .pipe( $.less() )
-        .pipe( $.cleanCss( {
-            format: {
-                breaks: { // controls where to insert breaks
-                    afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
-                    afterRuleEnds: true, // controls if a line break comes after a rule ends; defaults to `false`
-                }
-            }
-        } ) )
-        .pipe( $.sourcemaps.write() )
-        .pipe( $.rename( function ( path ) {
-            var dirname = path.dirname;
-            path.dirname = dirname.replace( 'scss', 'css/' );
-            path.basename = init.output.basename;
-        } ) )
-        .pipe( $.gulp.dest( 'jcr_root/' ) );
+	return $.gulp.src( 'jcr_root/**/less/{*,}/' )
+			.pipe( $.sourcemaps.init() )
+			.pipe( $.less() )
+			.pipe( $.cleanCss( {
+								   format: {
+									   breaks: { // controls where to insert breaks
+										   afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
+										   afterRuleEnds:  true // controls if a line break comes after a rule ends; defaults to `false`
+									   }
+								   }
+							   } ) )
+			.pipe( $.sourcemaps.write() )
+			.pipe( $.rename( function ( path ) {
+				var dirname   = path.dirname;
+				path.dirname  = dirname.replace( 'scss', 'css/' );
+				path.basename = init.output.basename;
+			} ) )
+			.pipe( $.gulp.dest( 'jcr_root/' ) );
 } );
-
 $.gulp.task( 'clean:js', function () {
-    return $.gulp.src( init.source.path.root + init.clean.path.js )
-        .pipe( $.clean() );
+	return $.gulp.src( init.source.path.root + init.clean.path.js )
+			.pipe( $.clean() );
 } );
-
 $.gulp.task( 'clean:css', function () {
-    return $.gulp.src( init.source.path.root + init.clean.path.css )
-        .pipe( $.clean() );
+	return $.gulp.src( init.source.path.root + init.clean.path.css )
+			.pipe( $.clean() );
 } );
-
-$.gulp.task( 'deploy:clientlibJS', function() {
-    return $.gulp.src('jcr_root/**/js/*.js')
-            .pipe( $.slang() );
-});
-
-$.gulp.task( 'deploy:clientlibCSS', function() {
-    return $.gulp.src('jcr_root/**/css/*.css')
-            .pipe( $.slang() );
-});
-
-$.gulp.task( 'deploy:componentHTML', function() {
-    return $.gulp.src('jcr_root/**/components/**/*.html')
-            .pipe( $.slang() );
-});
-
-$.gulp.task( 'deploy:componentJS', function() {
-    return $.gulp.src(['jcr_root/**/*.js', '!jcr_root/**/clientlib/**/*.js'])
-            .pipe( $.slang() );
-});
-
-
+$.gulp.task( 'deploy:clientlibJS', function () {
+	return $.gulp.src( 'jcr_root/**/js/*.js' )
+			.pipe( $.slang() );
+} );
+$.gulp.task( 'deploy:clientlibCSS', function () {
+	return $.gulp.src( 'jcr_root/**/css/*.css' )
+			.pipe( $.slang() );
+} );
+$.gulp.task( 'deploy:componentHTML', function () {
+	return $.gulp.src( 'jcr_root/**/components/**/*.html' )
+			.pipe( $.slang() );
+} );
+$.gulp.task( 'deploy:componentJS', function () {
+	return $.gulp.src( [ 'jcr_root/**/*.js', '!jcr_root/**/clientlib/**/*.js' ] )
+			.pipe( $.slang() );
+} );
 $.gulp.task( 'compile', [ 'compile:js', 'compile:sass' ] );
-$.gulp.task( 'clean', [ 'clean:js', 'clean:css' ] )
-$.gulp.task( 'deploy', [ 'deploy:clientlibJS', 'deploy:clientlibCSS', 'deploy:componentHTML','deploy:componentJS'] )
+$.gulp.task( 'clean', [ 'clean:js', 'clean:css' ] );
+$.gulp.task( 'deploy', [ 'deploy:clientlibJS', 'deploy:clientlibCSS', 'deploy:componentHTML', 'deploy:componentJS' ] );
